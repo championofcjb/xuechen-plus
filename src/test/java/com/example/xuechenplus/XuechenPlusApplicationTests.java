1d441f96ec77b36d966da1bfc149de0d35a7e527
photographt11 <photographt11@gamil.com>
2025-04-16 13:32:30 +0800

    init

---
+++ src/test/java/com/example/xuechenplus/XuechenPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xuechenplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechenPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
